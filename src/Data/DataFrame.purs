eb8e4abae17f69f6b69eb795ea1ae12690423f70
Mrinal Purohit <github@mrinalpurohit.in>
2018-12-20 12:18:22 +0530

    Add show instance for `DataFrame (Record a)` type

--- src/Data/DataFrame.purs
+++ src/Data/DataFrame.purs
@@ -2,13 +2,14 @@ module Data.DataFrame where
 
 import Prelude
 
-import Control.Apply (lift2)
+import Control.Apply (lift3)
 import Data.Compactable (class Compactable, separate)
+import Data.DataFrame.Helper (class RecToRow, recToRow)
 import Data.Filterable (class Filterable, partitionMap)
 import Data.Foldable (intercalate, maximum, surround)
 import Data.Function (on)
 import Data.Generic.Rep (class Generic)
-import Data.List (catMaybes, head, mapMaybe, partition, sortBy, take, transpose)
+import Data.List (List(..), catMaybes, head, mapMaybe, partition, sortBy, take, transpose)
 import Data.List (filter, length) as List
 import Data.List.Types (List)
 import Data.Map (Map)
@@ -17,8 +18,11 @@ import Data.Maybe (fromMaybe, maybe)
 import Data.Monoid (power)
 import Data.Newtype (class Newtype, over, unwrap, wrap)
 import Data.String (length)
-import Data.String.NonEmpty (NonEmptyString)
+import Data.String.NonEmpty (NonEmptyString, toString)
 import Data.Tuple (fst, snd)
+import Prim.RowList (class RowToList, kind RowList)
+import Type.Row (RLProxy(..))
+import Types (Row)
 
 newtype DataFrame a = DataFrame (List a)
 
@@ -44,30 +48,42 @@ instance filterableDF :: Filterable DataFrame where
   partition f = (\{yes, no} -> {yes: wrap yes, no: wrap no}) <<< partition f <<< unwrap
   partitionMap f = (\{left, right} -> {left: wrap left, right: wrap right}) <<< partitionMap f <<< unwrap
 
-instance showUntypedDataFrame :: Show a => Show (DataFrame (Map NonEmptyString a)) where
+getColumns :: List (List String) -> String
+getColumns (Cons x y) = (surround " | " x) <> "\n"
+getColumns Nil = ""
+
+instance showRecordDataFrame ::
+  ( RowToList a rl
+  , RecToRow rl a (Map NonEmptyString String)
+  ) => Show (DataFrame (Record a)) where
+  show rec = show $ (over DataFrame (map (recToRow (RLProxy :: RLProxy rl))) rec :: DataFrame (Row String))
+
+instance showUntypedDataFrame :: Show (DataFrame (Map NonEmptyString String)) where
   show =
     unwrap >>>
-    (lift2 (<>) (take 10 >>> showSummary) showRemainingCount)
+    lift3 (\title summary remaining -> title <> summary <> remaining) column (take 10 >>> showSummary) showRemainingCount
     where
-    showSummary =
-      map (Map.toUnfoldable >>> sortBy (compare `on` fst))
-      >>> transpose
-      >>> map drawColumn
-      >>> transpose
-      >>> map (surround " | ")
-      >>> (flip intercalate <*> head >>> maybe 0 length >>> power "-" >>> (_ <> "\n"))
-    drawColumn xs =
+    getField f = map (Map.toUnfoldable >>> sortBy (compare `on` fst)) >>> transpose >>> map (drawColumn f) >>> transpose
+    column = getField (fst >>> toString) >>> getColumns
+    getValue = (flip intercalate <*> (head >>> maybe 0 length >>> power "-" >>> append "\n"))
+    showSummary = getField snd
+      >>> map (surround " | " >>> append "\n")
+      >>> getValue
+    drawColumn f xs =
       map
-        (snd >>> padTo (max (maxLengthOfValues xs) (maybe 0 (fst >>> show >>> length) (head xs))))
+        (f >>> padTo (max (maxLengthOfValues xs) (maybe 0 (fst >>> show >>> length) (head xs))))
         xs
     maxLengthOfValues =
       map (snd >>> show >>> length)
       >>> maximum
       >>> fromMaybe 0
-    padTo :: Int -> a -> String
+    padTo :: Int -> String -> String
     padTo i a =
-      let pad = power " " $ (i - length (show a)) / 2
-      in pad <> show a <> pad
+      let textPad = i - length a
+          padVal = textPad / 2
+          padL = power " " padVal
+          padR = power " " $ textPad - padVal
+      in padL <> a <> padR
     showRemainingCount =
       List.length >>>
       (_ - 10) >>>
