e8bce648247471b9b68b84f2727944eeba0aa0f9
Arun raj <arun.raj@juspay.in>
2018-12-21 18:03:59 +0530

    fixed table view for Dataframe

--- src/Data/DataFrame.purs
+++ src/Data/DataFrame.purs
@@ -6,10 +6,10 @@ import Control.Apply (lift2, lift3)
 import Data.Compactable (class Compactable, separate)
 import Data.DataFrame.Helper (class RecToRow, recToRow)
 import Data.Filterable (class Filterable, partitionMap)
-import Data.Foldable (intercalate, maximum, surround)
+import Data.Foldable (maximum, surround)
 import Data.Function (on)
 import Data.Generic.Rep (class Generic)
-import Data.List (List(..), catMaybes, head, mapMaybe, partition, sortBy, take, transpose)
+import Data.List (List(..), catMaybes, fold, head, mapMaybe, partition, take, transpose, zipWith)
 import Data.List (filter, length) as List
 import Data.List.Types (List)
 import Data.Map (Map)
@@ -19,7 +19,7 @@ import Data.Monoid (power)
 import Data.Newtype (class Newtype, over, unwrap, wrap)
 import Data.String (length)
 import Data.String.NonEmpty (NonEmptyString, toString)
-import Data.Tuple (fst, snd)
+import Data.Tuple (Tuple, fst, snd)
 import Prim.RowList (class RowToList, kind RowList)
 import Type.Row (RLProxy(..))
 import Types (Row)
@@ -48,9 +48,6 @@ instance filterableDF :: Filterable DataFrame where
   partition f = (\{yes, no} -> {yes: wrap yes, no: wrap no}) <<< partition f <<< unwrap
   partitionMap f = (\{left, right} -> {left: wrap left, right: wrap right}) <<< partitionMap f <<< unwrap
 
-getColumns :: List (List String) -> String
-getColumns (Cons x y) = surround " | " x
-getColumns Nil = ""
 
 instance showRecordDataFrame ::
   ( RowToList a rl
@@ -60,31 +57,69 @@ instance showRecordDataFrame ::
 
 instance showUntypedDataFrame :: Show (DataFrame (Map NonEmptyString String)) where
   show =
-    unwrap >>>
-    lift3 (\title summary remaining -> title <> summary <> remaining) column (take 10 >>> showSummary) showRemainingCount
+    unwrap >>> lift3 append3 column showSummary showRemainingCount
+
     where
-    getField f = map (Map.toUnfoldable >>> sortBy (compare `on` fst)) >>> transpose >>> map (drawColumn f) >>> transpose
-    column = getField (fst >>> toString) >>> getColumns >>> lift2 append identity (length >>> power "-" >>> append "\n")
-    getValue = (flip intercalate <*> (head >>> maybe 0 length >>> power "-" >>> append "\n"))
-    showSummary = getField snd
-      >>> map (surround " | " >>> append "\n")
-      >>> getValue
-    drawColumn f xs =
-      map
-        (f >>> padTo (max (maxLengthOfValues xs) (maybe 0 (fst >>> toString >>> length) (head xs))))
-        xs
-    maxLengthOfValues =
-      map (snd >>> length)
-      >>> maximum
-      >>> fromMaybe 0
-    padTo :: Int -> String -> String
-    padTo i a =
-      let textPad = i - length a
-          padVal = textPad / 2
-          padL = power " " padVal
-          padR = power " " $ textPad - padVal
-      in padL <> a <> padR
-    showRemainingCount =
-      List.length >>>
-      (_ - 10) >>>
-      ((\l -> if _ then "\n... and " <> show l <> " more" else mempty) <*> (_ > 0))
+      getField f =
+        take 10
+        >>> (<$>) (Map.toUnfoldable)
+        >>> transpose
+        >>> (<$>) (drawColumn f)
+        >>> transpose
+
+      column =
+        getField (fst >>> toString)
+        >>> getColumns
+        >>> lift3
+              append3
+              identity
+              (const "\n")
+              (drawHL (flip sub 3) >>> surroundString "+")
+
+      showSummary :: List (Row String) -> String
+      showSummary = getField snd
+        >>> lift2
+              (zipWith (<>))
+              ((<$>) (surround "| " >>> surroundString "\n"))
+              (map ((map (drawHL (add 1))) >>> (surround "+")))
+        >>> fold
+
+      drawColumn :: ((Tuple NonEmptyString String) -> String)  -> List (Tuple NonEmptyString String) -> List String
+      drawColumn f xs =
+        map
+          (f >>> padTo (max (maxLengthOfValues xs) (maybe 0 (fst >>> toString >>> length) (head xs))))
+          xs
+
+      maxLengthOfValues :: List(Tuple NonEmptyString String) -> Int
+      maxLengthOfValues =
+        map (snd >>> length)
+        >>> maximum
+        >>> fromMaybe 0
+
+      padTo :: Int -> String -> String
+      padTo i a =
+        let textPad = i - length a
+            padVal = textPad / 2
+            padL = power " " padVal
+            padR = power " " $ textPad - padVal
+        in padL <> a <> " " <> padR
+
+      showRemainingCount :: List (Row String) -> String
+      showRemainingCount =
+        List.length >>>
+        (_ - 10) >>>
+        ((\l -> if _ then "\n... and " <> show l <> " more" else mempty) <*> (_ > 0))
+
+      getColumns :: List (List String) -> String
+      getColumns (Cons x y) = surround "| " x
+      getColumns Nil = ""
+
+      surroundString :: ∀ a. Semigroup a ⇒ a → a → a
+      surroundString x = flip (append3 x) x
+
+      drawHL :: (Int → Int) → String → String
+      drawHL fn = length >>> fn >>> power "-"
+
+
+append3 :: ∀ a. Semigroup a ⇒ a → a → a → a
+append3 = ((<<<) (<<<) (<>)) >>> ((>>>) (<>))
