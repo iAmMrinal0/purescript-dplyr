8cc38cd0977260c086219eb194aab44060a4dd7f
Arun raj <arun.raj@juspay.in>
2018-12-21 18:27:40 +0530

    remove quotes for string in table output file

--- src/Data/Helper.purs
+++ src/Data/Helper.purs
@@ -5,7 +5,7 @@ import Prelude
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe, maybe)
-import Data.String.NonEmpty (NonEmptyString, nes)
+import Data.String.NonEmpty (class MakeNonEmpty, NonEmptyString, nes)
 import Prim.Row (class Cons)
 import Prim.RowList (kind RowList)
 import Prim.RowList as RL
@@ -15,19 +15,35 @@ import Type.Prelude (class IsSymbol, RLProxy(..), SProxy(..))
 class RecToRow (xs :: RowList) (row :: # Type) to | xs -> to where
   recToRow :: RLProxy xs -> {|row} -> to
 
-instance recToRowCons ::
+instance recToRowStringCons ::
+  ( IsSymbol sym
+  , Cons sym (Maybe String) trash src
+  , RecToRow tail src (Map NonEmptyString String)
+  ) => RecToRow (RL.Cons sym (Maybe String) tail) src (Map NonEmptyString String) where
+  recToRow _ src = mkMap src (SProxy :: SProxy sym) (RLProxy :: RLProxy tail) identity
+
+else instance recToRowOtherCons ::
   ( IsSymbol sym
   , Show ty
   , Cons sym (Maybe ty) trash src
   , RecToRow tail src (Map NonEmptyString String)
   ) => RecToRow (RL.Cons sym (Maybe ty) tail) src (Map NonEmptyString String) where
-  recToRow _ src =
-    Map.insert (nes prop) value $ recToRow tail' src
-    where
-      focus = get prop src :: Maybe ty
-      value = maybe "" show focus
-      prop = SProxy :: SProxy sym
-      tail' = RLProxy :: RLProxy tail
+  recToRow _ src = mkMap src (SProxy :: SProxy sym) (RLProxy :: RLProxy tail) show
 
 else instance recToRowNil :: RecToRow RL.Nil src (Map NonEmptyString String) where
   recToRow _ _ = Map.empty
+
+mkMap :: ∀ trash src ty sym rl
+  . IsSymbol sym
+  ⇒ Cons sym (Maybe ty) trash src
+  ⇒ MakeNonEmpty sym
+  ⇒ RecToRow rl src (Map NonEmptyString String)
+  ⇒ { | src }
+  → SProxy sym
+  → RLProxy rl
+  → (ty → String)
+  → Map NonEmptyString String
+mkMap src sym tail f = Map.insert (nes sym) value $ recToRow tail src
+  where
+        focus = get sym src
+        value = maybe "" f focus
